2eecee42a2ea7ab80cbb0ba98c526709e5a50e7a
walkandrun <cz593682855@163.com>
2017-04-15 16:49:47 +0800

    新增两篇博客

--- bulid.js
+++ bulid.js
@@ -2,10 +2,13 @@ const fs = require('fs') // flie system
 const path = require('path')
 
 fs.readdir('./markdown',function(error,files){
-	var p = path.join('./markdown',files[0])
-	var markdown = fs.readFileSync(p).toString()
-	var template = fs.readFileSync('./template.html').toString()
-	var result = template.replace('%content%',markdown)
-	// console.log(markdown)
-	fs.writeFileSync(file[0]+'.html',result )
+	for(var i=0; i<files.length; i++){
+		var p = path.join('./markdown',files[i])
+		var markdown = fs.readFileSync(p).toString()
+		var template = fs.readFileSync('./template.html').toString()
+		var result = template.replace('%content%',markdown)
+		// console.log(markdown)
+		fs.writeFileSync(files[i]+'.html',result )
+	}
+	
 }) 
